adcb41245b4e9e47077a4c0ba5c81c7f9e7bad2f
Catarina Bengtsson <catarinabengtsson@Catarinas-MacBook-Pro.local>
2024-08-25 15:55:24 +0200

    New design on About me page
    Updated the triangels, squares and circles images.
    Fixed small spelling mistakes.
    Added more meta tags.
    And probably something more...

--- about.js
+++ about.js
@@ -1,15 +1,15 @@
 // Header animation
-window.addEventListener("load", function() {
-    setTimeout(function() {
-        document.getElementById("signature").classList.add('show');
-      }, 2500);
-});
+// window.addEventListener("load", function() {
+//     setTimeout(function() {
+//         document.getElementById("signature").classList.add('show');
+//       }, 2500);
+// });
 
-setTimeout(function() {
+setTimeout(function () {
     const hiddenElements = document.querySelectorAll('.anchor');
     hiddenElements.forEach((el) => observer.observe(el));
 }, 300);
-  
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
--- navbar.js
+++ navbar.js
@@ -3,10 +3,19 @@ var navLinks = document.getElementById("navLinks")
 
 function showMenu() {
     navLinks.style.right = "0";
-    // navLinks.style.top = "0";
 }
 
 function hideMenu() {
     navLinks.style.right = "-200px";
-    // navLinks.style.top = "-300px";
+}
+
+function toggleMenu() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+        console.log("block");
+        x.style.display = "none";
+    } else {
+        console.log("else");
+        x.style.display = "block";
+    }
 }
